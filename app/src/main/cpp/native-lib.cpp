d295b55db15b4de35779da7439d22404ec4a18db
wzq <695445360@qq.com>
2017-10-11 23:23:38 +0800

    so包

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,11 +1,11 @@
 #include <jni.h>
 #include <string>
-
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_wzq_ffmpegdemo_MainActivity_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */) {
+Java_com_wzq_ffmpegdemo_ui_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
+
+    // TODO
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
